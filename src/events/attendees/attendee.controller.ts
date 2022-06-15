40c5f2672ea01c9d59a0c24cebff281409e80680
Ramses Dijkstra <ramses@sigbar.nl>
2022-06-15 13:50:45 +0100

    #1942 Added add and remove attendee

--- src/events/attendees/attendee.controller.ts
+++ src/events/attendees/attendee.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Get, HttpStatus, Param, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpStatus,
+  Param,
+  Post,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SpaceIntegrationUsersService } from '../../space-integration-users/space-integration-users.service';
 import { EventsGuard } from '../events.guard';
@@ -8,13 +17,23 @@ import { IntegrationType } from '../../integration-type/integration-type.entity'
 import { IntegrationTypeService } from '../../integration-type/integration-type.service';
 import { IntegrationTypes } from '../../integration-type/integration-type.interface';
 import { TokenInterface } from '../../auth/auth.interface';
-
-export interface AttendeeAddDto {}
+import { User } from '../../user/user.entity';
+import { UserService } from '../../user/user.service';
+import { bytesToUuid, uuidToBytes } from '../../utils/uuid-converter';
+import { Space } from '../../space/space.entity';
+import { SpaceService } from '../../space/space.service';
+import { SpaceIntegrationsService } from '../../space-integrations/space-integrations.service';
 
 @ApiTags('attendees')
 @Controller('attendees')
 export class AttendeeController {
-  constructor(private readonly spaceIntegrationUserService: SpaceIntegrationUsersService, private readonly integrationTypeService: IntegrationTypeService) {}
+  constructor(
+    private readonly spaceIntegrationService: SpaceIntegrationsService,
+    private readonly spaceIntegrationUserService: SpaceIntegrationUsersService,
+    private readonly integrationTypeService: IntegrationTypeService,
+    private readonly userService: UserService,
+    private readonly spaceService: SpaceService,
+  ) {}
 
   @ApiBearerAuth()
   @ApiResponse({
@@ -22,18 +41,21 @@ export class AttendeeController {
     type: ResponseEventDto,
   })
   @UseGuards(EventsGuard)
-  @Post('add')
-  async addAttendee(@Body() body: AttendeeAddDto, @Req() request: TokenInterface, @Res() res): Promise<any> {
+  @Post('add/:spaceId')
+  async addAttendee(@Param() params, @Req() request: TokenInterface, @Res() res): Promise<any> {
     try {
       const integrationType: IntegrationType = await this.integrationTypeService.findOne(IntegrationTypes.EVENT);
+      const user: User = await this.userService.findOne(uuidToBytes(request.user.sub));
+      const space: Space = await this.spaceService.findOne(uuidToBytes(params.spaceId));
 
       const spaceIntegrationUser = new SpaceIntegrationUser();
       spaceIntegrationUser.integrationTypeId = integrationType.id;
-      spaceIntegrationUser.userId = integrationType.id;
-      spaceIntegrationUser.spaceId = integrationType.id;
-      const sIU = await this.spaceIntegrationUserService.create(params.spaceId, params.eventId);
+      spaceIntegrationUser.userId = user.id;
+      spaceIntegrationUser.spaceId = space.id;
 
-      res.status(HttpStatus.OK).json(event);
+      const sIU = await this.spaceIntegrationUserService.create(spaceIntegrationUser);
+
+      res.status(HttpStatus.OK).json(sIU);
     } catch (e) {
       console.log(e);
       res.status(HttpStatus.INTERNAL_SERVER_ERROR).json({ error: e.message });
@@ -46,15 +68,24 @@ export class AttendeeController {
     type: ResponseEventDto,
   })
   @UseGuards(EventsGuard)
-  @Post('remove')
+  @Post('remove/:spaceId')
   async removeAttendee(@Param() params, @Req() request: TokenInterface, @Res() res): Promise<any> {
     try {
-      const event = await this.eventsService.getOne(params.spaceId, params.eventId);
+      const integrationType: IntegrationType = await this.integrationTypeService.findOne(IntegrationTypes.EVENT);
+      const space: Space = await this.spaceService.findOne(uuidToBytes(params.spaceId));
+      const user: User = await this.userService.findOne(uuidToBytes(request.user.sub));
+      const spaceIntegration = await this.spaceIntegrationService.findOneBySpaceAndIntegration(space, integrationType);
 
-      if (!event) {
-        res.status(HttpStatus.NOT_FOUND).json({ error: `event with Id=${params.eventId} not found` });
+      const sIU = await this.spaceIntegrationUserService.findDistinct(spaceIntegration, user);
+
+      if (!sIU) {
+        res
+          .status(HttpStatus.NOT_FOUND)
+          .json({ error: `siu for Id=${bytesToUuid(spaceIntegration.spaceId)} not found` });
         return;
       }
+
+      await this.spaceIntegrationUserService.delete(sIU);
 
       res.status(HttpStatus.OK).json(event);
     } catch (e) {
